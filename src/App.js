0fb89c9a1ea7b2c5a2e89caa80c5fcd54e4a477b
Your Name <you@example.com>
2019-08-10 22:32:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person' ;
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hey im react</h1> 
+        <Person />     
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
+
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react' ;
+
+
+const person = () => {
+return <p> I'm Yefet </p>
+
+};
+
+export default person ;
